150acfb89a08320d8aa4e20abafd292dc85b9670
unknown <ed12rivera@gmail.com>
2021-02-22 00:45:32 -0800

    Adding files for mapping Toronto flight data.

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -2,13 +2,13 @@
 console.log("working");
 
 // Create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
 // Create the street view tile layer that will be an option for the map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -16,15 +16,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Light: light,
     Dark: dark
 };
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44, -80],
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 });
 
 // Pass the map layers into the layers control and add the layers control to the map.
@@ -34,10 +34,10 @@ L.control.layers(baseMaps).addTo(map);
 // streets.addTo(map);
 
 // Accessing the airport GeoJSON url.
-let airportData = "https://raw.githubusercontent.com/ed12rivera/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json"
+let torontoData = "file:///C:/Users/Edward/Documents/Berkeley_Boot_Camp/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 
 // Grab the GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
     // function onEachFeature(feature, layer) {
